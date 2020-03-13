af75684cb428720ff9679113247d2812732cc2ee
Gerry Whelan <gerry.whelan@version1.com>
2020-03-13 10:08:48 +0000

    sync on 13 03

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const express = require("express"), //note express is not a variable but a funct
   homeController = require("./controllers/homeController"),
   petController = require("./controllers/petController"),
   
-  layouts = require("express-ejs-layouts"), //load express EJS layouts and assign to a variable layouts
+  layouts = require("express-ejs-layouts"), //load express EJS layouts and assign to a variable 
+  // layouts which is then used later in this file
   
   mongoose = require("mongoose"),
   
@@ -15,7 +16,7 @@ const express = require("express"), //note express is not a variable but a funct
 
 mongoose.Promise = global.Promise;
 
-/* MongoDB stuff for Azure
+/* MongoDB stuff
 mongoose.connect(
     "mongodb://dogpaw-db:Pz1aO7zh5i2zVcZ9O8VRxMjKffbcXt8cXbUzNUNYI5f3ySGNgS97BxQ0m4nOaqgNmc5HN1Fd10Z69K3pN8grDg==@dogpaw-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",
   { useNewUrlParser: true, 
@@ -29,29 +30,19 @@ db.once("open", () => {
   console.log("Successfully connected to MongoDB using Mongoose!");
 });
 */
-
-mongoose.connect(
-  "mongodb://localhost:27017"     
-);
-mongoose.set("useCreateIndex", true);
-const db = mongoose.connection;
-
-db.once("open", () => {
-console.log("Successfully connected to MongoDB using Mongoose!");
-});
-
-
 app.set("port", process.env.PORT || 3000); //read from file or default to port 3000
-app.set("view engine", "ejs"); //use EJS engine for generating views for later sending to browser
+app.set("view engine", "ejs"); //use EJS view engine for viewing. could be any one of 20 different view engines
+//ones available on npmjs.com 
 
-app.use(express.static("public")); //use static files from the public folder - images, CSS, HTML
-app.use(layouts); //layouts created earlier. here we tell app object about it.
+app.use(express.static("public")); //use static files from the public folder - things like images, 
+// CSS, HTML files. These are files that don't ever change so application just presents them as they are
+app.use(layouts); //layouts variable created earlier. here we tell app to use it. app object controls everything
 app.use(
   express.urlencoded({
     extended: false
   })
 );
-app.use(express.json()); //could split out and store in variable, then assign to app
+app.use(express.json()); //could split out and store in variable, then assign to app. this is one step to do both
 app.use(homeController.logRequestPaths);
 
 //app.get("/name", homeController.respondWithName);
@@ -59,19 +50,23 @@ app.use(homeController.logRequestPaths);
 
 app.get("/petprofile", petController.getPetProfile, (req, res, next) => {
   res.render("petprofile", { petprofile: req.data });
-});
+}); //when someone types or links to http://localhost:3000/petprofile in browser then go to call getPetProfile() method which
+//gets the pet profile and stores it in the request object req. parameter iii of above then uses this data 
+//to send back to web page  
+//for the previous - ****NOTHING*** is happening just now. its just setting this up for when the app is running. 
 
-app.get("/", homeController.index);
-app.get("/courses", homeController.showCourses);
+app.get("/", homeController.index); //when http://localhost:3000/ is called just call homeController.index
+app.get("/courses", homeController.showCourses); //when call http://localhosr:3000/courses call..
 
 //app.get("/contact", subscribersController.getSubscriptionPage);
 //app.post("/subscribe", subscribersController.saveSubscriber);
-//test change
 
 app.use(errorController.logErrors);
 app.use(errorController.respondNoResourceFound);
 app.use(errorController.respondInternalError);
 
+//following code starts the up the web server and the app is now running 
+//and waiting for requests via browser
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
