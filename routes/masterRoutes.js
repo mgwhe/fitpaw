b5907528ac27eadcdb319f4c17248993cd168590
Gerry Whelan <gerrywhelan@Gerrys-MacBook-Pro.local>
2020-05-09 13:02:38 +0100

    Minor changes to setup

--- routes/masterRoutes.js
+++ routes/masterRoutes.js
@@ -4,6 +4,7 @@ const router = require("express").Router(),
 
 masterController = require("../controllers/masterController"); 
 
+//need to code out
 //supress menu option to edit/update pet profile
 //router.get("/no_pet_profile", masterController.getMasterPageNoPetProfile);
 //otherwise..
